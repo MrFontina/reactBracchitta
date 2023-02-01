2bdd28cf3af3cd2b1e7132f36570e95df3f2549d
Nahuel Bracchitta <nahuelbracchitta@gmail.com>
2023-01-31 23:04:40 -0300

    mejoras de estilo en cards

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import Button1 from "../Button1/Button1"
 import { Link } from "react-router-dom"
 import "./item.css"
@@ -11,25 +11,30 @@ function Item(product) {
     setIsFavorite(!isFavorite);
   }
 
-  let classButtonFavorite = 
+  let classButtonFavorite =
     isFavorite === true ? "card-favicon favorite" : "card-favicon";
 
   let urlDetail = `/detalle/${product.id}`
 
-  let stylePrice = { color: product.discount? 'green' : 'inherit' }
+  let stylePrice = { color: product.discount ? 'green' : 'inherit' }
 
   return (
-    <div className='card'>             
+    <div className='card'>
+      <div className="cardContent">
         <div className="cardTop">
-            <h2>{product.title}</h2>
-            <div className='card-details'>
-            <Link to={urlDetail}><img src={product.img} alt="producto imagen"/></Link>
+          <h2>{product.title}</h2>
+          <div className='card-details'>
+            <Link to={urlDetail}><img src={product.img} alt="producto imagen" /></Link>
+          </div>
+          <h4 style={stylePrice}>${product.price}</h4>
         </div>
-            <h4 style={ stylePrice }>${product.price}</h4>
-            {product.discount ? <h4 className="offer">Descuento: {product.discount}%</h4> : <></>}
-        </div>        
-       <button onClick={handleFavorite} className={classButtonFavorite}>♥</button><br /><br />
-       <Link to={urlDetail}><Button1 className="btnVer" width="120px" weight="bold" size="15px" color="green">Ver más!</Button1></Link>       
+        <div className="cardBottom">
+          {product.discount ? <h4 className="offer">Descuento: {product.discount}%</h4> : <></>}
+        <button onClick={handleFavorite} className={classButtonFavorite}>♥</button><br /><br />
+        <Link to={urlDetail}><Button1 className="btnVer" width="120px" weight="bold" size="15px" color="green">Ver más!</Button1></Link>
+        </div>
+        
+      </div>
     </div>
   )
 }
